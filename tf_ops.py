bbb582e2f1ff53722a159eacc9a2f301d350c3e9
Fergal Cotter <fbcotter90@gmail.com>
2017-08-09 02:02:12 +0100

    Fix bug in version info take 2
    Needed to change the path to absolute path. Obvs. Instead of
    open('./version.py'), now made open(os.path.join(__file__) ...)

--- tf_ops.py
+++ tf_ops.py
@@ -6,12 +6,14 @@ from __future__ import print_function
 
 import tensorflow as tf
 import math
+import os
 import warnings
 from tensorflow.python.layers import convolutional, normalization
 from tensorflow.python.ops import init_ops
 
 __author__ = "Fergal Cotter"
-exec(open("./version.py").read())  # Imports the __version__ variable
+# Imports the __version__ variable
+exec(open(os.path.join(os.path.dirname(__file__), 'version.py')).read())
 __version_info__ = tuple([int(d) for d in __version__.split(".")])  # noqa
 
 
