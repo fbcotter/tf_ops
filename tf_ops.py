1f449c5c93ee976a13ed48cb4477fe7f8c8d3318
Fergal Cotter <fbcotter90@gmail.com>
2017-08-09 01:33:39 +0100

    Add version info to repo

--- tf_ops.py
+++ tf_ops.py
@@ -1,13 +1,18 @@
+""" A collection of helper tf functions.
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
 import math
-import numpy as np
 import warnings
-from tensorflow.python.layers import convolutional, normalization, core
+from tensorflow.python.layers import convolutional, normalization
 from tensorflow.python.ops import init_ops
+
+__author__ = "Fergal Cotter"
+exec(open('version.py').read())  # Imports the __version__ variable
+__version_info__ = tuple([int(d) for d in '.'.split(__version__)])  # noqa
 
 
 def variable_with_wd(name, shape, stddev=None, wd=None, norm=2):
@@ -64,7 +69,7 @@ def variable_summaries(var, name='summaries'):
 
     Parameters
     ----------
-    var : tf variable
+    var : :py:class:`tf.Tensor`
         variable for which you wish to create summaries
     name : str
         scope under which you want to add your summary ops
@@ -128,7 +133,7 @@ def convolution(x, output_dim, size=3, stride=1, stddev=None, wd=None,
 
     Parameters
     ----------
-    x : tf variable
+    x : :py:class:`tf.Tensor`
         The input variable
     output_dim : int
         number of filters to have
@@ -213,7 +218,7 @@ def convolution_transpose(x, output_dim, shape, size=3, stride=1,
 
     Parameters
     ----------
-    x : tf variable
+    x : :py:class:`tf.Tensor`
         The input variable
     output_dim : int
         number of filters to have
@@ -239,11 +244,10 @@ def convolution_transpose(x, output_dim, shape, size=3, stride=1,
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying complex convolution transpose to x
     """
-    warnings.warn("Deprecated. Use tf.python.layers.convolution.Conv2DTranspose",
-                  DeprecationWarning)
+    warnings.warn("Deprecated. Use tf.python.layers", DeprecationWarning)
 
     varlist = []
     with tf.variable_scope(name):
@@ -280,7 +284,7 @@ def linear(x, output_dim, stddev=None, wd=0.01, norm=2, name='fc',
 
     Parameters
     ----------
-    x : tf variable
+    x : :py:class:`tf.Tensor`
         The input variable
     output_dim : int
         number of filters to have
@@ -320,7 +324,7 @@ def linear(x, output_dim, stddev=None, wd=0.01, norm=2, name='fc',
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying fully connected layer to x
     """
     warnings.warn("Deprecated. Use tf.python.layers.core.Dense",
@@ -452,8 +456,137 @@ def residual(x, filters, stride=1, train=True, wd=0.0001):
     return x + orig_x
 
 
-def lift_residual(x1, x2, filters, train=True, wd=0.0001):
-    """Define a Lifting Layer
+def lazy_wavelet(x):
+    """ Performs a lazy wavelet split on a tensor.
+
+    Designed to work nicely with the lifting blocks.
+
+    Output will have 4 times as many channels as the input, but will have one
+    quarter the spatial size.  I.e. if x is a tensor of size (batch, h, w, c),
+    then the output will be of size (batch, h/2, w/2 4*c). The first c channels
+    will be the A samples::
+
+        Input image
+
+        A B A B A B ...
+        C D C D C D ...
+        A B A B A B ...
+        ...
+
+    Then the next c channels will be from the B channels, and so on.
+
+    Notes
+    -----
+    If the spatial size is not even, then will mirror to make it even before
+    downsampling.
+
+    Parameters
+    ----------
+    x : tf tensor
+        Input to apply lazy wavelet transform to.
+
+    Returns
+    -------
+    y : tf tensor
+        Result after applying transform.
+    """
+    xshape = x.get_shape().as_list()
+    pad = [[0,0], [0,0], [0,0], [0,0]]
+    if xshape[1] % 2 == 1:
+        pad[1][1] = 1
+    if xshape[2] % 2 == 1:
+        pad[2][1] = 1
+    x = tf.pad(x, pad, 'SYMMETRIC')
+    A, B = x[:, ::2, ::2], x[:, ::2, 1::2]
+    C, D = x[:, 1::2, ::2], x[:, 1::2, 1::2]
+    y = tf.concat([A, B, C, D], axis=-1)
+    return y
+
+
+def lazy_wavelet_inv(x, out_size=None):
+    """ Performs the inverse of a lazy wavelet transform - a 'lazy recombine'
+
+    Designed to work nicely with the lifting blocks.
+
+    Output will have 1/4 as many channels as the input, but will have one
+    quadruple the spatial size.  I.e. if x is a tensor of size (batch, h, w, c),
+    then the output will be of size (batch, 2*h, 2*w c/4). If we call the first
+    c channels the A group, then the second c the B group, and so on, the output
+    image will be interleaved like so::
+
+        Output image
+
+        A B A B A B ...
+        C D C D C D ...
+        A B A B A B ...
+        ...
+
+    Notes
+    -----
+    If the forward lazy wavelet needed padding, then we should be undoing it
+    here. For this, specify the out_size of the resulting tensor.
+
+    Parameters
+    ----------
+    x : tf tensor
+        Input to apply lazy wavelet transform to.
+    out_size : tuple of 4 ints or None
+        What the output size should be of the resulting tensor. The batch size
+        will be ignored, but the spatial and channel size need to be correct.
+        For an input spatial size of (h, r), the spatial dimensions (the 2nd and
+        3rd numbers in the tuple) should be either (2*h, 2*r),
+        (2*h-1, 2*r), (2*h, 2*r-1) or (2*h-1, 2*r-1). Will raise a ValueError if
+        not one of these options. Can also be None, in which (2*h, 2*r) is
+        assumed. The channel size should be 1/4 of the input channel size.
+
+    Returns
+    -------
+    y : tf tensor
+        Result after applying transform.
+
+    Raises
+    ------
+    ValueError when the out_size is invalid, or if the input tensor's channel
+    dimension is not divisible by 4.
+    """
+    xshape = x.get_shape().as_list()
+    # Check the channel axis
+    if xshape[-1] % 4 != 0:
+        raise ValueError('Input tensor needs to have 4k channels')
+    if xshape[-1] // 4 != out_size[-1]:
+        raise ValueError('Out tensor needs to have 1/4 channels of input')
+    k = xshape[-1] // 4
+
+    # Check the spatial axes
+    dbl_size = [2*xshape[1], 2*xshape[2]]
+    if out_size is None:
+        out_size = dbl_size
+    else:
+        out_size = out_size[1:3]
+    if out_size[0] != dbl_size[0] and out_size[0] != dbl_size[0] - 1:
+        raise ValueError('Row size in out_size incorrect')
+    if out_size[1] != dbl_size[1] and out_size[1] != dbl_size[1] - 1:
+        raise ValueError('Col size in out_size incorrect')
+
+    A, B = x[:, :, :, :k], x[:, :, :, k:2*k]
+    C, D = x[:, :, :, 2*k:3*k], x[:, :, :, 3*k:]
+
+    # Create the even and odd rows by careful stacking and reshaping.
+    r_o = tf.stack([A, B], axis=3)
+    r_o = tf.reshape(r_o, [-1, xshape[1], xshape[2]*2, k])
+    r_e = tf.stack([C, D], axis=3)
+    r_e = tf.reshape(r_e, [-1, xshape[1], xshape[2]*2, k])
+    y = tf.stack([r_o, r_e], axis=2)
+    y = tf.reshape(y, [-1, xshape[1]*2, xshape[2]*2, k])
+
+    # Cut off the edges if need be.
+    y = y[:, :out_size[0], :out_size[1], :]
+    return y
+
+
+def lift_residual_resample(x1, x2, filters, train=True, downsize=True,
+                           wd=0.0001):
+    """Define a Lifting Layer with resizing
 
     The P and the U blocks for this lifting layer are non-linear functions.
     These are the same form as the F(x) in a residual layer (i.e.  two
@@ -479,7 +612,7 @@ def lift_residual(x1, x2, filters, train=True, wd=0.0001):
     x2 : tf tensor
         Input tensor 2
     filters : int
-        Number of output channels for Px2 and Ud
+        Number of output channels for P*x2 and U*d
     train : bool or tf boolean tensor
         Whether we are in the train phase or not. Can set to a tensorflow tensor
         so that it can be modified on the fly.
@@ -493,6 +626,128 @@ def lift_residual(x1, x2, filters, train=True, wd=0.0001):
     s : tf tensor
         Scale coefficients
     """
+    assert x1.get_shape().as_list() == x2.get_shape().as_list()
+
+    # Split the two inputs
+    if downsize:
+        x1 = lazy_wavelet(x1)
+        x2 = lazy_wavelet(x2)
+
+    in_channels = x1.get_shape().as_list()[-1]
+    if (filters % in_channels != 0):
+        raise ValueError('Can only expand an image by an integer number ' +
+                         'of its channels')
+
+    # If the requested number of output channels is larger than the input,
+    # stack the input to make them match.
+    if filters != in_channels:
+        # We've already checked that the remainder is 0 so can do integer div
+        nstack = filters // in_channels
+        x1 = tf.concat([x1]*nstack, axis=-1)
+
+    d, s = lift_residual(x1, x2, train, wd)
+
+    return d, s
+
+
+def lift_residual_resample_inv(d, s, out_size, train=True, wd=0.0001):
+    """Define a inverse Lifting Layer with resizing
+
+    The P and the U blocks for this lifting layer are non-linear functions.
+    These are the same form as the F(x) in a residual layer (i.e.  two
+    convolutions). In block form, a lifting layer looks like this::
+
+    We share the variables with the forward lifting.
+
+    In block form, the inverse lifting layer looks like this (note the sign swap
+    and flow direction reversal compared to the forward case)::
+
+             _______________
+            |               |
+        x1<-|---(+)---------|<-d
+            |    ^      |   |
+            |    |      |   |
+            |   ---    ---  |
+            |  | P |  |-U | |
+            |   ---    ---  |
+            |    |      |   |
+            |    |      v   |
+        x2<-|----------(+)--|<-s
+            |_______________|
+
+    Parameters
+    ----------
+    d : tf tensor
+        Input tensor 1
+    s : tf tensor
+        Input tensor 2
+    out_size : list of ints
+        Size of the resulting x1 tensors.
+    train : bool or tf boolean tensor
+        Whether we are in the train phase or not. Can set to a tensorflow tensor
+        so that it can be modified on the fly.
+    wd : float
+        Weight decay term for the convolutional weights
+
+    Returns
+    -------
+    x1 : tf tensor
+        Reconstructed x1
+    x2 : tf tensor
+        Reconstructed x2
+    """
+    assert d.get_shape().as_list() == s.get_shape().as_list()
+    x1, x2 = lift_residual_inv(d, s, train, wd)
+
+    # Recombine the two outputs
+    if out_size[-1] != x1.get_shape().as_list()[-1]:
+        x1 = lazy_wavelet_inv(x1, out_size)
+        x2 = lazy_wavelet_inv(x2, out_size)
+
+    return x1, x2
+
+
+def lift_residual(x1, x2, train=True, wd=0.0001):
+    """Define a Lifting Layer
+
+    The P and the U blocks for this lifting layer are non-linear functions.
+    These are the same form as the F(x) in a residual layer (i.e.  two
+    convolutions). In block form, a lifting layer looks like this::
+
+             _______________
+            |               |
+        x1->|---(+)---------|->d
+            |    ^      |   |
+            |    |      |   |
+            |   ---    ---  |
+            |  |-P |  | U | |
+            |   ---    ---  |
+            |    |      |   |
+            |    |      v   |
+        x2->|----------(+)--|->s
+            |_______________|
+
+    Parameters
+    ----------
+    x1 : tf tensor
+        Input tensor 1
+    x2 : tf tensor
+        Input tensor 2
+    train : bool or tf boolean tensor
+        Whether we are in the train phase or not. Can set to a tensorflow tensor
+        so that it can be modified on the fly.
+    wd : float
+        Weight decay term for the convolutional weights
+
+    Returns
+    -------
+    d : tf tensor
+        Detail coefficients
+    s : tf tensor
+        Scale coefficients
+    """
+    filters = x1.get_shape().as_list()[-1]
+    assert filters == x2.get_shape().as_list()[-1]
     with tf.variable_scope('P'):
         # Calculate d = x1 - Px2
         d = x1 - _residual_core(x2, filters, 1, train, wd)
@@ -504,7 +759,7 @@ def lift_residual(x1, x2, filters, train=True, wd=0.0001):
     return d, s
 
 
-def lift_residual_inv(d, s, filters, train=True, wd=0.0001):
+def lift_residual_inv(d, s, train=True, wd=0.0001):
     """Define the inverse of a lifting layer
 
     We share the variables with the forward lifting.
@@ -547,6 +802,8 @@ def lift_residual_inv(d, s, filters, train=True, wd=0.0001):
     x2 : tf tensor
         Reconstructed x2
     """
+    filters = d.get_shape().as_list()[-1]
+    assert filters == s.get_shape().as_list()[-1]
     with tf.variable_scope('U') as scope:
         # Calculate x2 = s - Ud
         scope.reuse_variables()
@@ -574,7 +831,7 @@ def complex_convolution(x, output_dim, size=3, stride=1, stddev=None,
 
     Parameters
     ----------
-    x : tf variable
+    x : :py:class:`tf.Tensor`
         The input variable
     output_dim : int
         number of filters to have
@@ -601,7 +858,7 @@ def complex_convolution(x, output_dim, size=3, stride=1, stddev=None,
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying complex convolution to x
     """
 
@@ -646,7 +903,7 @@ def complex_convolution_transpose(x, output_dim, shape, size=3, stride=1,
 
     Parameters
     ----------
-    x : tf variable
+    x : :py:class:`tf.Tensor`
         The input variable
     output_dim : int
         number of filters to have
@@ -670,7 +927,7 @@ def complex_convolution_transpose(x, output_dim, shape, size=3, stride=1,
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying complex convolution transpose to x
     """
 
@@ -697,7 +954,7 @@ def cconv2d(x, w, **kwargs):
     """ Performs convolution with complex inputs and weights
 
     Need to create the weights and feed to this function. If you want to have
-    this done for you automatically, use :py:func:`complex_convolution`
+    this done for you automatically, use :py:func:`complex_convolution`.
 
     Parameters
     ----------
@@ -710,8 +967,12 @@ def cconv2d(x, w, **kwargs):
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying convolution to x
+
+    Notes
+    -----
+    Uses tf.nn.conv2d which I believe is actually cross-correlation.
     """
     default_args = {
         'strides': [1, 1, 1, 1],
@@ -726,6 +987,8 @@ def cconv2d(x, w, **kwargs):
         else:
             default_args[key] = val
 
+    x = tf.cast(x, tf.complex64)
+    w = tf.cast(w, tf.complex64)
     x_r = tf.real(x)
     x_i = tf.imag(x)
     w_r = tf.real(w)
@@ -742,7 +1005,7 @@ def cconv2d_transpose(y, w, output_shape, **kwargs):
 
     Need to create the weights and feed to this function. If you want to have
     this done for you automatically, use
-    :py:func:`complex_convolution_transpose`
+    :py:func:`complex_convolution_transpose`.
 
     Parameters
     ----------
@@ -755,11 +1018,12 @@ def cconv2d_transpose(y, w, output_shape, **kwargs):
 
     Notes
     -----
-    Takes the complex conjugate of w before doing convolution.
+    Takes the complex conjugate of w before doing convolution. Uses
+    tf.nn.conv2d_transpose which I believe is actually convolution.
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying convolution to x
     """
     default_args = {
@@ -776,6 +1040,8 @@ def cconv2d_transpose(y, w, output_shape, **kwargs):
         else:
             default_args[key] = val
 
+    y = tf.cast(y, tf.complex64)
+    w = tf.cast(w, tf.complex64)
     y_r = tf.real(y)
     y_i = tf.imag(y)
     w_r = tf.real(w)
@@ -798,7 +1064,7 @@ def separable_conv_with_pad(x, h_row, h_col, stride=1):
 
     Parameters
     ----------
-    x : tf variable of shape [Batch, height, width, c]
+    x : :py:class:`tf.Tensor` of shape [Batch, height, width, c]
         The input variable. Should be of shape
     h_row : tf tensor of shape [1, l, c_in, c_out]
         The spatial row filter
@@ -809,7 +1075,7 @@ def separable_conv_with_pad(x, h_row, h_col, stride=1):
 
     Returns
     -------
-    y : tf variable
+    y : :py:class:`tf.Tensor`
         Result of applying convolution to x
     """
     # Do the row filter first:
@@ -999,7 +1265,7 @@ def real_reg(w, wd=0.01, norm=2):
 
     Parameters
     ----------
-    w : tf variable
+    w : :py:class:`tf.Tensor`
         The weights to regularize
     wd : positive float, optional (default=0.01)
         Regularization parameter
@@ -1008,7 +1274,7 @@ def real_reg(w, wd=0.01, norm=2):
 
     Returns
     -------
-    reg_loss : tf variable
+    reg_loss : :py:class:`tf.Tensor`
         The loss. This method does not add anything to the REGULARIZATION_LOSSES
         collection. The calling function needs to do that.
 
@@ -1050,7 +1316,7 @@ def complex_reg(w, wd=0.01, norm=1):
 
     Parameters
     ----------
-    w : tf variable (dtype=complex)
+    w : :py:class:`tf.Tensor` (dtype=complex)
         The weights to regularize
     wd : positive float, optional (default=0.01)
         Regularization parameter
@@ -1059,7 +1325,7 @@ def complex_reg(w, wd=0.01, norm=1):
 
     Returns
     -------
-    reg_loss : tf variable
+    reg_loss : :py:class:`tf.Tensor`
         The loss. This method does not add anything to the REGULARIZATION_LOSSES
         collection. The calling function needs to do that.
 
